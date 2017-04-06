7adaf172732d5c3602c43a5b9040cd8f3729e4ca
ip2176 <ip2176@g.rit.edu>
2017-04-06 14:49:27 -0400

    Add a layer of abstraction for the tuition payment

--- subsystems/financial_aid/handler.py
+++ subsystems/financial_aid/handler.py
@@ -1,5 +1,6 @@
 from .forms import PaymentForm
-from tornado import web, template
+from tornado import web, template, escape
+import json
 import os
 
 balance = 126000
@@ -19,21 +20,37 @@ class FinancialAidHandler(web.RequestHandler):
         payment_success = False
         result = template_loader.load("financial_aid.html").generate(title="[RMS] Financial Aid",
                                                                      form=form, balance=balance,
-                                                                     payment_success=payment_success)
+                                                                     payment_success=payment_success,
+                                                                     make_payment=False)
         self.write(result)
 
     def post(self):
         global balance
         global payment_success
-        form = PaymentForm(self.request.arguments)
-        if form.validate():
-            balance = balance - form.data['amount']
-            payment_success = True
+        body = self.request.body.decode()
+
+        # The user selected to make a payment, show them the form
+        if 'make_payment' in body:
+            form = PaymentForm()
+            payment_success = False
             result = template_loader.load("financial_aid.html").generate(title="[RMS] Financial Aid",
                                                                          form=form, balance=balance,
-                                                                         amount=form.data['amount'],
-                                                                         payment_success=payment_success)
+                                                                         payment_success=payment_success,
+                                                                         make_payment=True)
             self.write(result)
+
+        # The user submitted the form, check it
         else:
-            self.set_status(400)
-            self.write("There were errors on the form: {}".format(form.errors))
+            form = PaymentForm(self.request.arguments)
+            if form.validate():
+                balance = balance - form.data['amount']
+                payment_success = True
+                result = template_loader.load("financial_aid.html").generate(title="[RMS] Financial Aid",
+                                                                             form=form, balance=balance,
+                                                                             amount=form.data['amount'],
+                                                                             payment_success=payment_success,
+                                                                             make_payment=True)
+                self.write(result)
+            else:
+                self.set_status(400)
+                self.write("There were errors on the form: {}".format(form.errors))
